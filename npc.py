8aac044656ff2a4a0b7c4a8f8eb3b30f4c2e6ce9
Andrei Benea <andrei.benea89@gmail.com>
2025-11-27 12:05:48 -0500

    integrate dialogue hints in npc.talk

--- npc.py
+++ npc.py
@@ -1,3 +1,10 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from game import Game
+
+
 class NonPlayerCharacter:
     def __init__(self, id: str, name: str, scene_id: str, dialog_id=None):
         self.id = id
@@ -6,11 +13,18 @@ class NonPlayerCharacter:
         self.dialog_id = dialog_id
         self.type = "npc"
 
-    def talk(self, intro: str, potential_replies: dict):
-        lines = [intro]
+    def talk(self, game: "Game"):
+        lines = [game.dialog_intro]
+        dialog_hint = f"{game.talking_to}_{self.scene_id}"
         i = 1
-        for reply_key in potential_replies:
-            line = f"{i}. {potential_replies[reply_key][self.type]}"
+        for reply_key in game.dialog_options:
+            if dialog_hint in game.scene_hints:
+                if game.dialog_options[reply_key]["outcome"] == "good":
+                    line = f"{i}. [HINT] {game.dialog_options[reply_key][self.type]}"
+                else:
+                    line = f"{i}. {game.dialog_options[reply_key][self.type]}"
+            else:
+                line = f"{i}. {game.dialog_options[reply_key][self.type]}"
             lines.append(line)
             i += 1
         return lines
